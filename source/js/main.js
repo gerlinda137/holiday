37d8342d60efc581d143cea6f3808e0bb1d4b905
gerlinda137 <gerlinda137@gmail.com>
2020-12-01 14:10:55 +0300

    mobile-menu

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,10 @@ var mySwiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
   spaceBetween: 60,
 });
+
+let nav = document.querySelector('.main-nav');
+let burger = nav.querySelector('.main-nav__burger');
+
+burger.onclick = function() {
+  nav.classList.toggle('main-nav--menu-opened')
+};
