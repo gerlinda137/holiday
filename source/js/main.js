4144719e2cebf9feb7f61119974fc2fbf76a9c40
gerlinda137 <gerlinda137@gmail.com>
2020-12-01 22:24:49 +0300

    about us tablet

--- source/js/main.js
+++ source/js/main.js
@@ -10,3 +10,43 @@ let burger = nav.querySelector('.main-nav__burger');
 burger.onclick = function() {
   nav.classList.toggle('main-nav--menu-opened')
 };
+
+
+//sliders on resize
+const advantages = document.querySelector('.features__wrapper');
+
+
+let varietySwiper;
+let audienceSwiper;
+
+
+let advantagesSwiper;
+
+function mobileSlider() {
+	if (window.innerWidth <= 767 && advantages.dataset.mobile == 'false') {
+		advantagesSwiper = new Swiper(advantages, {
+			slidesPerView: 'auto',
+			spaceBetween: 30,
+			slideClass: 'advantages-card',
+			// pagination: {
+			// 	el: '.swiper-pagination',
+			// 	clickable: true,
+			// },
+		});
+
+		advantages.dataset.mobile = 'true';
+	}
+
+	if (window.innerWidth > 767) {
+		advantages.dataset.mobile = 'false';
+		if (advantages.classList.contains('swiper-container-initialized')) {
+			advantagesSwiper.destroy();
+		}
+	}
+}
+
+mobileSlider()
+
+window.addEventListener('resize', () => {
+	mobileSlider();
+});
