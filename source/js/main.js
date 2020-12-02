7541a6fc083d284357af059a6e2041875878cddd
gerlinda137 <gerlinda137@gmail.com>
2020-12-02 17:51:40 +0300

    trying to rebuild slider

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.programmes__slider', {
   spaceBetween: 10,
   slidesPerView: 'auto',
   spaceBetween: 60,
@@ -13,40 +13,90 @@ burger.onclick = function() {
 
 
 //sliders on resize
-const advantages = document.querySelector('.features__wrapper');
 
 
-let varietySwiper;
-let audienceSwiper;
+
+// let varietySwiper;
+// let audienceSwiper;
 
 
-let advantagesSwiper;
+// let advantagesSwiper;
 
-function mobileSlider() {
-	if (window.innerWidth <= 767 && advantages.dataset.mobile == 'false') {
-		advantagesSwiper = new Swiper(advantages, {
-			slidesPerView: 'auto',
-			spaceBetween: 30,
-			slideClass: 'advantages-card',
-			// pagination: {
-			// 	el: '.swiper-pagination',
-			// 	clickable: true,
-			// },
-		});
+// function mobileSlider() {
+// 	if (window.innerWidth <= 767 && advantages.dataset.mobile == 'false') {
+// 		advantagesSwiper = new Swiper(advantages, {
+// 			slidesPerView: 1,
+// 			spaceBetween: 30,
+// 			slideClass: 'features__item',
+// 			// pagination: {
+// 			// 	el: '.swiper-pagination',
+// 			// 	clickable: true,
+// 			// },
+// 		});
 
-		advantages.dataset.mobile = 'true';
-	}
+// 		advantages.dataset.mobile = 'true';
+// 	}
 
-	if (window.innerWidth > 767) {
-		advantages.dataset.mobile = 'false';
-		if (advantages.classList.contains('swiper-container-initialized')) {
-			advantagesSwiper.destroy();
-		}
-	}
+// 	if (window.innerWidth > 767) {
+// 		advantages.dataset.mobile = 'false';
+// 		if (advantages.classList.contains('swiper-container-initialized')) {
+// 			advantagesSwiper.destroy();
+// 		}
+// 	}
+// }
+
+// mobileSlider()
+
+// window.addEventListener('resize', () => {
+// 	mobileSlider();
+// });
+const advantages = document.querySelector('.features');
+const pageWidthMobileThreshold = 767;
+
+let advantagesSwiper = null;
+
+function checkCreateAdvantagesMobileSwiper() {
+  console.log('inner ' + window.innerWidth);
+  // return;
+  if (window.innerWidth <= pageWidthMobileThreshold) {
+
+    console.log('malo');
+    if (advantagesSwiper === null) {
+      console.log('create');
+
+      advantagesSwiper = new Swiper(advantages, {
+        // Optional parameters
+        // direction: 'vertical',
+
+        // If we need pagination
+        spaceBetween: 20,
+        // slidesPerView: 'auto',
+        // width: 400,
+        // centeredSlides: true,
+        // visibilityFullFit: true,
+
+        // pagination: {
+        //   el: '.sale-chairs__pagination',
+        //   type: 'progressbar',
+        // },
+
+        wrapperClass: 'features__wrapper',
+
+        slideClass: 'features__item',
+
+    })
+    }
+
+  } else {
+    console.log('mnogo');
+    if (advantagesSwiper !== null) {
+      console.log('destroy');
+      advantagesSwiper.destroy();
+      advantagesSwiper = null;
+    }
+  }
 }
 
-mobileSlider()
 
-window.addEventListener('resize', () => {
-	mobileSlider();
-});
+checkCreateAdvantagesMobileSwiper();
+window.addEventListener('resize', checkCreateAdvantagesMobileSwiper);
